bf3174f12cb002a171312a13fbbdefb60a44aa95
lea-stellini <lea.stellini@gmail.com>
2022-11-29 15:01:29 +0100

    added icons

--- src/Components/App.js
+++ src/Components/App.js
@@ -7,7 +7,7 @@ function App() {
   const [data, setData] = useState({});
   const [location, setLocation] = useState('');
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&APPID=844c13f6bda60d25e077e1ba71204f9e`
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&APPID=844c13f6bda60d25e077e1ba71204f9e`;
 
   const searchLocation = (event) => {
       if(event.key === 'Enter') {
@@ -35,7 +35,7 @@ function App() {
         <div className="result">
             <h2>{data.name}</h2>
             {data.main ? <h3>{data.main.temp.toFixed()}ºC</h3> : null}
-            {data.weather ? <p>{data.weather[0].main}</p> : null}
+            {data.weather ? <img src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}  className="icon"/> : null}
        </div> 
         </div>
     </div>
